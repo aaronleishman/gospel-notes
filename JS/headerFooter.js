ec7595bece5fab53beba7ed216a154517673db4b
Aaron Leishman <aaronleishman@gmail.com>
2024-11-15 09:22:57 -0600

    v.20241115.1

--- JS/headerFooter.js
+++ JS/headerFooter.js
@@ -3,19 +3,29 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <p style="text-align: left;">
-        <ul>
-            <li><a href="./index.html">Home</a></li>
-            <li><a href="./ComeFollowMe.html">Come Follow Me</a></li>
-            <li><a href="./Covenants.html">Covenants</a></li>
-            <li><a href="./GeneralConference.html">General Conference</a></li>
-            <li><a href="./PlanOfSalvation.html">Plan of Salvation</a></li>
-            <li><a href="./Talks.html">Sacramenting Talks</a></li>
-            <li><a href="./Temples.html">Temples</a></li>
-        </ul>
+            <div style="margin: 1rem;">
+            <a href="http://aaronleishman.com/gospel-notes/ComeFollowMe.html">Come Follow Me</a> |
+            <a href="http://aaronleishman.com/gospel-notes/Covenants.html">Covenants</a> |
+            <a href="http://aaronleishman.com/gospel-notes/GeneralConference.html">General Conference</a> |
+            <a href="http://aaronleishman.com/gospel-notes/PlanOfSalvation.html">Plan of Salvation</a> |
+            <a href="http://aaronleishman.com/gospel-notes/Talks.html">Sacrament Mtg Talks</a> |
+            <a href="http://aaronleishman.com/gospel-notes/Temples.html">Temples</a>
+            </div>
+            <hr>
         </p>
     `
     }
 }
 
 customElements.define('my-header', MyHeader)
+
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+    <hr>
+        <span>@ 2024 | Contact Me</span>
+    `
+    }
+}
+
+customElements.define('my-footer', MyFooter)
