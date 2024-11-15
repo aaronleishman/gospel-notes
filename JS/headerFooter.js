42ac2203ab6f23007cb36b539727ad4e8259398d
Aaron Leishman <aaronleishman@gmail.com>
2024-11-15 10:36:10 -0600

    v.20241115.4

--- JS/headerFooter.js
+++ JS/headerFooter.js
@@ -4,12 +4,12 @@ class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div style="margin: 1rem;">
-            <a href="http://aaronleishman.com/gospel-notes/ComeFollowMe.html">Come Follow Me</a> |
-            <a href="http://aaronleishman.com/gospel-notes/Covenants.html">Covenants</a> |
-            <a href="http://aaronleishman.com/gospel-notes/GeneralConference.html">General Conference</a> |
-            <a href="http://aaronleishman.com/gospel-notes/PlanOfSalvation.html">Plan of Salvation</a> |
-            <a href="http://aaronleishman.com/gospel-notes/Talks.html">Sacrament Mtg Talks</a> |
-            <a href="http://aaronleishman.com/gospel-notes/Temples.html">Temples</a>
+            <a href="./ComeFollowMe.html">Come Follow Me</a> |
+            <a href="./Covenants.html">Covenants</a> |
+            <a href="./GeneralConference.html">General Conference</a> |
+            <a href="./PlanOfSalvation.html">Plan of Salvation</a> |
+            <a href="./Talks.html">Sacrament Mtg Talks</a> |
+            <a href="./Temples.html">Temples</a>
             </div>
             <hr>
         </p>
@@ -18,6 +18,28 @@ class MyHeader extends HTMLElement {
 }
 
 customElements.define('my-header', MyHeader)
+
+class ArticleHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div style="margin: 1rem;">
+            <a href="../ComeFollowMe.html">Come Follow Me</a> |
+            <a href="../Covenants.html">Covenants</a> |
+            <a href="../GeneralConference.html">General Conference</a> |
+            <a href="../PlanOfSalvation.html">Plan of Salvation</a> |
+            <a href="../Talks.html">Sacrament Mtg Talks</a> |
+            <a href="../Temples.html">Temples</a> |
+            <br>
+            <a href="../index.html">Home</a> |
+            <a href="#" onclick="history.back()">Back</a>
+            </div>
+            <hr>
+        </p>
+    `
+    }
+}
+
+customElements.define('article-header', ArticleHeader)
 
 class MyFooter extends HTMLElement {
     connectedCallback() {
